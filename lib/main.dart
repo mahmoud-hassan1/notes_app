b87ad3e40ff66200c2f82012b3ac6446d6a16cd8
mahmoud <mahmoudhassan10191019@gmail.com>
2024-07-08 15:27:42 +0300

    login view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_app/features/auth/presentation/views/login_view.dart';
 import 'package:notes_app/firebase_options.dart';
 
 void main() async{
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      
+      home: const LoginView(),
     );
   }
 }

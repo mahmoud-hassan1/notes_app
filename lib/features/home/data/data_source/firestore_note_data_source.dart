5dae83b8d6b9f66e875ebf1115c7e9960da445a4
mahmoud <mahmoudhassan10191019@gmail.com>
2024-07-08 15:07:44 +0300

    fire store data source

---
+++ lib/features/home/data/data_source/firestore_note_data_source.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:notes_app/features/home/data/models/note_model.dart';
+
+class FirestoreNoteDataSource {
+  final FirebaseFirestore firestore;
+
+  FirestoreNoteDataSource({required this.firestore});
+
+  Future<void> addNote(NoteModel note) async {
+    await firestore.collection('notes').add(note.toMap());
+  }
+
+  Stream<List<NoteModel>> getNotes(String uid) {
+    return firestore.collection('notes')
+      .where('uid', isEqualTo: uid)
+      .snapshots()
+      .map((snapshot) => snapshot.docs.map((doc) => NoteModel.fromFirestore(doc)).toList());
+  }
+
+}

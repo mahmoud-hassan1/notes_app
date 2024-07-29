29283d697301be3d1b8ab6b4af7446f7ac22ed04
mahmoud <mahmoudhassan10191019@gmail.com>
2024-07-29 11:18:46 +0300

    check responsiveness

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notes_app/core/utiles/app_colors.dart';
@@ -10,7 +11,10 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const MyApp());
+  runApp( DevicePreview(
+    enabled: true,
+    builder: (context) => const MyApp(), // Wrap your app
+  ),);
 }
 
 class MyApp extends StatelessWidget {
